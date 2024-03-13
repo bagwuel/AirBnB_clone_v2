575373371339bd30072c24935eb4daa9ce7e5a96
bagwuel <mbagwu39@gmail.com>
2024-03-13 01:30:57 +0100

    task 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,28 @@ from datetime import datetime
 
 @task
 def do_pack():
-    """generates a .tgz archive from web_static"""
-    datestr = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(datestr)
-    local("mkdir -p versions")
-    if local("tar -cvzf {} web_static/".format(file_name)).succeeded:
-        return file_name
-    return None
+    """generates a .tgz archive from the contents of the web_static folder"""
+    try:
+        # Create a timestamp for the archive name
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        
+        # Define the name of the archive
+        archive_name = "web_static_{}.tgz".format(timestamp)
+        
+        # Create the folder versions if it doesn't exist
+        local("mkdir -p versions")
+        
+        # Compress the contents of the web_static folder into the archive
+        local("tar -cvzf versions/{} web_static".format(archive_name))
+        
+        # Return the path to the generated archive
+        return "versions/{}".format(archive_name)
+    
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+
+if __name__ == "__main__":
+    do_pack()
+
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,39 +3,32 @@
 from fabric.decorators import task
 from fabric.api import *
 from datetime import datetime
-import os
+from os import path
 
-env.hosts = ["18.207.236.32", "52.201.228.254"]
-
+env.user = 'ubuntu'
+env.hosts = ['18.207.236.32', '52.201.228.254']
 
 @task
 def do_deploy(archive_path):
-    """Fabric script that distributes an archive to web servers"""
-    try:
-        if not os.path.exists(archive_path):
-            return False
-        with_ext = archive_path.split("/")[-1]
-        without_ext = archive_path.split("/")[-1].split(".")[0]
-        pth = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("mkdir -p " + pth + without_ext)
-        run("tar -xzf /tmp/{} -C {}{}/".format(with_ext, pth, without_ext))
-        run("rm /tmp/{}".format(with_ext))
-        run("mv {1}{0}/web_static/* {1}{0}/".format(without_ext, pth))
-        run("rm -rf {}{}/web_static".format(pth, without_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(pth, without_ext))
-        return True
-    except Exception:
+    """Distributes an archive to web servers"""
+    if not path.exists(archive_path):
         return False
 
+    try:
+        put(archive_path, '/tmp')
+        filename = archive_path.split('/')[-1]
+        foldername = filename.split('.')[0]
+        release_path = '/data/web_static/releases/' + foldername
 
-@task
-def do_pack():
-    """generates a .tgz archive from web_static"""
-    datestr = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(datestr)
-    local("mkdir -p versions")
-    if local("tar -cvzf {} web_static/".format(file_name)).succeeded:
-        return file_name
-    return None
+        run('sudo mkdir -p {}'.format(release_path))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(filename, release_path))
+        run('sudo rm /tmp/{}'.format(filename))
+        run('sudo mv -f {}/web_static/* {}'.format(release_path, release_path))
+        run('sudo rm -rf {}/web_static'.format(release_path))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(release_path))
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
