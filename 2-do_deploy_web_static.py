a3066a1e089e5c7affe96dc424cd27c8e5e441d3
bagwuel <mbagwu39@gmail.com>
2024-03-13 01:37:14 +0100

    task-2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,32 +3,39 @@
 from fabric.decorators import task
 from fabric.api import *
 from datetime import datetime
-from os import path
+import os
 
-env.user = 'ubuntu'
-env.hosts = ['18.207.236.32', '52.201.228.254']
+env.hosts = ["18.207.236.32", "52.201.228.254"]
+
 
 @task
 def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
-    if not path.exists(archive_path):
-        return False
-
+    """Fabric script that distributes an archive to web servers"""
     try:
-        put(archive_path, '/tmp')
-        filename = archive_path.split('/')[-1]
-        foldername = filename.split('.')[0]
-        release_path = '/data/web_static/releases/' + foldername
-
-        run('sudo mkdir -p {}'.format(release_path))
-        run('sudo tar -xzf /tmp/{} -C {}'.format(filename, release_path))
-        run('sudo rm /tmp/{}'.format(filename))
-        run('sudo mv -f {}/web_static/* {}'.format(release_path, release_path))
-        run('sudo rm -rf {}/web_static'.format(release_path))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(release_path))
-
+        if not os.path.exists(archive_path):
+            return False
+        with_ext = archive_path.split("/")[-1]
+        without_ext = archive_path.split("/")[-1].split(".")[0]
+        pth = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p " + pth + without_ext)
+        run("sudo tar -xzf /tmp/{} -C {}{}/".format(with_ext, pth, without_ext))
+        run("sudo rm /tmp/{}".format(with_ext))
+        run("sudo mv {1}{0}/web_static/* {1}{0}/".format(without_ext, pth))
+        run("sudo rm -rf {}{}/web_static".format(pth, without_ext))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}{}/ /data/web_static/current".format(pth, without_ext))
         return True
-    except Exception as e:
-        print(e)
+    except Exception:
         return False
+
+
+@task
+def do_pack():
+    """generates a .tgz archive from web_static"""
+    datestr = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(datestr)
+    local("sudo mkdir -p versions")
+    if local("sudo tar -cvzf {} web_static/".format(file_name)).succeeded:
+        return file_name
+    return None
